31d777884bd141007cdf299cf984c9e6e2348d6e
古俣祐斗 <komayuto1999@gmail.com>
2020-10-31 16:50:26 +0900

    新規登録、ログインのviewを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def display_name
+    profile&.nickname || self.email.split('@').first
+  end
+
 end
